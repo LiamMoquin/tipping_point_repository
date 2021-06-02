f0066edbcf8e02d9bfdfd7bb6251764d1083d99f
Liammoquin <LiamMoquin06@gmail.com>
2021-06-01 18:42:39 -0700

    Motor/Controller Setup

--- tipping_point/src/robotconfig.cpp
+++ tipping_point/src/robotconfig.cpp
@@ -1,12 +1,12 @@
 #include "robotconfig.h"
 #include "main.h"
 
-pros::Controller master (CONTROLLER_MASTER);
-pros::Controller partner (CONTROLLER_PARTNER);
-pros::Motor lf (1, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
-pros::Motor rf (2, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
-pros::Motor lb (3, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
-pros::Motor rb (4, E_MOTOR_GEARSET_18, false, E_MOTOR_ENCODER_DEGREES);
+pros::Controller master (pros::E_CONTROLLER_MASTER);
+pros::Controller partner (pros::E_CONTROLLER_PARTNER);
+pros::Motor lf (1, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor rf (2, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor lb (3, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor rb (4, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 
 void vexcodeInit(void){
     //nothing to initialize
