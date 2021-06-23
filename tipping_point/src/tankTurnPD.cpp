31e35e879e93ed5bbc646eae83072a8ac820f401
LiamMoquin <Liammoquin06@gmail.com>
2021-06-23 13:32:36 -0700

    Update tankTurnPD.cpp

--- tipping_point/src/tankTurnPD.cpp
+++ tipping_point/src/tankTurnPD.cpp
@@ -9,8 +9,10 @@ double tkD = 0.8;
 
 void turn(double target)
 {
-    while(target - 1 < abs(imu_sensor.get_heading()) && abs(imu_sensor.get_heading()) < target + 1)
+    while(!(target - 1 < abs(imu_sensor.get_heading()) && abs(imu_sensor.get_heading()) < target + 1))
     {
+        error = target - imu_sensor.get_heading();
+        derivative = error - lastError;
 
     }
 }
