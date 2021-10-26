af8c687786e949a6285bd53ba3e3ac557c925f22
LiamMoquin <84659300+LiamMoquin@users.noreply.github.com>
2021-10-26 15:56:24 -0700

    Update robotconfig.cpp

--- tipping_point/src/robotconfig.cpp
+++ tipping_point/src/robotconfig.cpp
@@ -4,9 +4,9 @@
 pros::Imu imu_sensor(5);
 pros::Controller master (pros::E_CONTROLLER_MASTER);
 pros::Controller partner (pros::E_CONTROLLER_PARTNER);
-pros::Motor lf (1, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor lf (1, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor rf (2, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor lb (3, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor lb (3, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor rb (4, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
 
 void vexcodeInit(void){
