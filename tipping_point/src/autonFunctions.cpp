c01d63f6e6c08086ab7a0ff7235023afb5d1e728
LiamMoquin <84659300+LiamMoquin@users.noreply.github.com>
2021-11-23 17:57:25 -0800

    test of trig fun

--- tipping_point/src/autonFunctions.cpp
+++ tipping_point/src/autonFunctions.cpp
@@ -1,8 +1,11 @@
 #include "main.h"
 
-void trigCalc(double a, double o){
+void trigCalc(double a = 54, double o = 27){
+    
     double angle = tan(o/a);
-    double dist hypot(a, o);
+    double dist = hypot(a, o);
+    print(0, 0, angle);
+    print(1, 0, dist);
     //turn to angle
     //move dist
 }
