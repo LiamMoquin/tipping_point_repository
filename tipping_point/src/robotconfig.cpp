92962b17b22a72fbab6b2c2ac30b888039c4274b
LiamMoquin <84659300+LiamMoquin@users.noreply.github.com>
2021-12-09 18:38:29 -0800

    Update robotconfig.cpp

--- tipping_point/src/robotconfig.cpp
+++ tipping_point/src/robotconfig.cpp
@@ -1,11 +1,11 @@
 #include "main.h"
 
 //setting up motors and controllers
-pros::Imu imu_sensor(5);
+pros::Imu imu_sensor(10);
 pros::Controller master (pros::E_CONTROLLER_MASTER);
 pros::Controller partner (pros::E_CONTROLLER_PARTNER);
-pros::Motor lf (4, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor rf (3, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor lf (3, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor rf (4, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor lb (2, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor rb (1, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor mt1 (6, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
